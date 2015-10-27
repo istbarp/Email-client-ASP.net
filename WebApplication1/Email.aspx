8b9f66b097e5a7c7948ff0b8e95bb1abbe9c9548
Patrick Petersen <unic_patrick@hotmail.com>
2015-10-28 00:10:11 +0100

    Initial upload, working.

---
+++ WebApplication1/Email.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Email" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Email.aspx.cs" Inherits="WebApplication1.Email" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3> -- Email Page for test mail.</h3>
+    
+    <asp:Button ID="btnSubmit" OnClick="btnSubmit_Click" runat="server" Text="Send Test Email" />
+
+</asp:Content>
---
+++ WebApplication1/Email.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Email : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //send email
+            string Subject = "This is test mail using smtp settings";
+            string Body = "Hello this is a test";
+            string ToEmail = "unic_patrick@hotmail.com";
+            string SMTPUser = "istbarp@gmail.com", SMTPPassword = "";
+            SmtpClient smtp = new SmtpClient();
+
+            smtp.UseDefaultCredentials = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.EnableSsl = true;
+            MailMessage mail = new MailMessage();
+            mail.To.Add(ToEmail);
+            mail.From = new MailAddress(SMTPUser);
+            mail.Subject = Subject;
+            mail.Body = Body;
+            mail.IsBodyHtml = true;
+
+            //domain (can be changed from gmail to whatever
+            smtp.Host = "smtp.gmail.com";
+            //change your port for your host
+            smtp.Port = 587;
+            smtp.Credentials = new System.Net.NetworkCredential(SMTPUser, SMTPPassword);
+            //smtp.Host = "smtp.gmail.com";
+
+            //try catch for no errors.
+
+            smtp.Send(mail);              
+           
+
+        }
+    }
+}
